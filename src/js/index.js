6018d3c4a66d0164cdd06acb5c2181edfea27060
iddimsangi <adammsangi@gmail.com>
2020-03-25 09:21:05 +0300

    project setup with npm,webpack and babel

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+// import axios from 'axios';
+// async function getRecp(query) {
+//     const recpCall = await axios(`https://jsonplaceholder.typicode.com/todos/${query}`);
+// }
+// getRecp('1');
